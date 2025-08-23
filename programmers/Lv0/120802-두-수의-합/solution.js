7b54e7bed27987c99a88cd4639c5323325022c04
pung8146 <pung8146@naver.com>
2025-08-23 10:32:45 +0900

    first commit

---
+++ programmers/Lv0/120802-두-수의-합/solution.js
@@ -0,0 +1,13 @@
+// /programmers/120802-두-수의-합/solution.js
+
+/**
+ *  문제: 두 수의 합
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120802
+ *
+ * 나의 풀이: 간단한 덧셈 연산자로 해결.
+ * 배운 점: 프로그래머스 기본 함수 구조 파악.
+ */
+
+function solution(num1, num2) {
+  return num1 + num2;
+}
